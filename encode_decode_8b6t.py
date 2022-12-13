05cf20555d3aedb669635dea1786844cf7f37bf5
Eduardo <eduardo.as2000@hotmail.com>
2022-12-13 16:34:27 -0300

    adiciona grafico

--- encode_decode_8b6t.py
+++ encode_decode_8b6t.py
@@ -4,6 +4,7 @@ import pandas as pd
 class Encode_decode:
     def __init__(self, msg):
         self.msg = msg
+        self.encoded_message = ''
 
     def encode_8b6t(self):
         df = pd.read_csv("./table.csv")
@@ -13,8 +14,9 @@ class Encode_decode:
         for value in self.msg:
             index = int(value)
             encoded_msg.append(encoded_list[index])
-        print(encoded_msg)
-        return encoded_msg
+        msg = ''.join(value for value in encoded_msg)
+        self.encoded_message = msg
+        return msg
     
     def decode_8b6t(self, encoded_msg):
         df = pd.read_csv("./table.csv")
@@ -27,8 +29,25 @@ class Encode_decode:
         print(msg)
         return msg
 
-msg = [116, 101, 115, 116, 101, 44, 32, 111, 112, 97, 32, 49, 50, 33]
+    def get_graph(self):
+        values = []
+        for c in self.encoded_message:
+            if c == '+':
+                values.append(1)
+            elif c == '0':
+                values.append(0)
+            elif c == '-':
+                values.append(-1)
+        from matplotlib import pyplot as plt
+        plt.plot(values)
+        plt.show()
+        return None
+
+message = [116, 101, 115, 116, 101, 44, 32, 111, 112, 97, 32, 49, 50, 33]
 encoded_msg = ['000+0-', '++0-00', '000+00', '000+0-', '++0-00', '0++--0', '-++-00', '++0--+', '000++-', '+0+-00', '-++-00', '0+--+0', '0+-0-+', '+00+--']
-a = Encode_decode(msg)
+
+a = Encode_decode(message)
 encoded = a.encode_8b6t()
 encoded = a.decode_8b6t(encoded_msg)
+
+a.get_graph()
